feea19e52777ee3df0ccf04da658fa1e3f3ce426
Anna Sisenina <yuta4ka71@gmail.com>
2024-12-19 18:51:16 +0200

    task 12 with answer 1710

--- task12 copy.py
+++ task12 copy.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 data = []
 
 with open("input6.txt", "r") as file:
@@ -13,9 +15,7 @@ with open("input6.txt", "r") as file:
 num_rows = len(data)
 num_col = len (data[0])
 count = 0
-
-for string in data:
-    print (string)
+test = []
 
 right = False
 left = False
@@ -55,7 +55,7 @@ def check_right(p, q):
             break
         q += 1
     if check_pq:
-        return [check_pq, [p, q-1], [p, q]]
+        return [check_pq, [p, q-1], [p, q]] # True/False; point to go; coordinates of obstacle
     return [check_pq, [-1, -1], [-1, -1]]
 
 def check_down(p, q):
@@ -91,33 +91,98 @@ def check_up(p, q):
         return [True, [p+1, q], [p, q]]
     return [False, [-1, -1], [-1, -1]]
 
-def check_square (a, b, d, c):
-    if (a[0] - b[0] == d[0] - c[0] 
-                    ) and (
-                        a[1] - b[1] == d[1] - c[1] 
-                    ) and (
-                        a[0] - d[0] == b[0] - c[0] 
-                    ) and (
-                        a[1] - d[1] == b[1] - c[1] 
-                    ):
-        return True
-    return False
-        
+
+def check(dir, a, b):
+    global data
+    check_stuck = False
+    i = a
+    j = b
+    text = ""
+    return_i = -1
+    return_j = -1
+
+    direction = dir
+    obstacles = []
+    right, down, left, up = [True,-1,-1], [True,-1,-1], [True,-1,-1], [True,-1,-1]
+    if direction == "u":
+        obstacles.append([i-1,j])
+        text = data[i-1][j]
+        return_i = i-1
+        return_j = j
+        data[i-1][j] = "#"
+        direction = "r"
+    elif direction == "r":
+        obstacles.append([i,j+1])
+        text = data[i][j+1]
+        return_i = i
+        return_j = j+1
+        data[i][j+1] = "#"
+        direction = "d"
+    elif direction == "d":
+        obstacles.append([i+1,j])
+        text = data[i+1][j]
+        return_i = i+1
+        return_j = j
+        data[i+1][j] = "#"
+        direction = "l"
+    elif direction == "l":
+        obstacles.append([i,j-1])
+        text = data[i][j-1]
+        return_i = i
+        return_j = j-1
+        data[i][j-1] = "#"
+        direction = "u"
+
+    while len(obstacles) < 200 and not check_stuck and right[0] == True and down[0] == True and left[0] == True and up[0] == True:
+        if direction == "u":
+            up = check_up(i, j)
+            if up[0]:
+                obstacles.append(up[2])
+                direction = "r"
+                i = up[1][0]
+                j = up[1][1]
+        if direction == "r":    
+            right = check_right(i, j)
+            if right[0]:
+                obstacles.append(right[2])
+                direction = "d"
+                i = right[1][0]
+                j = right[1][1]
+        if direction == "d":
+            down = check_down(i, j)
+            if down[0]:
+                obstacles.append(down[2])
+                direction = "l"
+                i = down[1][0]
+                j = down[1][1]    
+        if direction == "l":
+            left = check_left(i, j)
+            if left[0]:
+                obstacles.append(left[2])
+                direction = "u"
+                i = left[1][0]
+                j = left[1][1]              
+            
+        if len(obstacles) > 4:
+            if len(obstacles) % 4 != 0 and obstacles[0][0] == obstacles[len(obstacles)-len(obstacles)%4][0] and obstacles[0][1] == obstacles[len(obstacles)-len(obstacles)%4][1]:
+                check_stuck = True
+            elif len(obstacles) % 4 == 0 and obstacles[0][0] == obstacles[len(obstacles) - 4][0] and obstacles[0][1] == obstacles[len(obstacles) - 4][1]:
+                check_stuck = True
+                
+
+    if check_stuck:
+        test.append([obstacles[0][0],obstacles[0][1]])
+    data[return_i][return_j]=text
+    return check_stuck
 
 def go_up():
     global i, j, count
+    
     while  i != 0 and data[i-1][j] != "#":
         if data[i][j] == ".":
             data[i][j] = "|"
-        right = check_right(i,j)
-        if right[0]:
-            down = check_down(*right[1])
-            if down[0]:
-                left = check_left(*down[1])
-                if left[0]:
-                    if check_square([i-1, j], right[2], left[2], down[2]):
-                        count += 1
-                        print(f"up: {i-1, j}, right: {right[2]}, left: {left[2]}, down: {down[2]}")
+        if check("u", i, j):
+            count += 1
         i -= 1
     if i > 0 and data[i-1][j] == "#":
         change_direction("r")
@@ -129,15 +194,8 @@ def go_right():
     while j != num_col-1 and data[i][j+1] != "#":
         if data[i][j] == ".":
             data[i][j] = "-"
-        down = check_down(i,j)
-        if down[0]:
-            left = check_left(*down[1])
-            if left[0]:
-                up = check_up(*left[1])
-                if up[0]:
-                    if check_square(up[2], [i, j+1], left[2], down[2]):
-                        count += 1
-                        print(f"up: {up[2]}, right: {[i, j+1]}, left: {left[2]}, down: {down[2]}")
+        if check("r", i, j):
+            count += 1
         j += 1
     if j < num_rows-1 and data[i][j+1] == "#":
         change_direction("d")
@@ -148,15 +206,8 @@ def go_down():
     while i != num_rows-1 and data[i+1][j] != "#":
         if data[i][j] == ".":
             data[i][j] = "|"
-        left = check_left(i,j)
-        if left[0]:
-            up = check_up(*left[1])
-            if up[0]:
-                right = check_right(*up[1])
-                if right[0]:
-                    if check_square(up[2], right[2], left[2], [i+1, j]):
-                        count += 1
-                        print(f"up: {up[2]}, right: {right[2]}, left: {left[2]}, down: {[i+1, j]}")
+        if check("d", i, j):
+            count += 1
         i += 1
     if i < num_rows-1 and data[i+1][j] == "#":
         change_direction("l")
@@ -166,15 +217,8 @@ def go_left():
     while j != 0 and data[i][j-1] != "#":
         if data[i][j] == ".":
             data[i][j] = "-"
-        up = check_up(i,j)
-        if up[0]:
-            right = check_right(*up[1])
-            if right[0]:
-                down = check_down(*right[1])
-                if down[0]:
-                    if check_square(up[2], right[2], [i, j-1], down[2]):
-                        count += 1
-                        print(f"up: {up[2]}, right: {right[2]}, left: {[i, j-1]}, down: {down[2]}")
+        if check("l", i, j):
+            count += 1
         j -= 1
     if j > 0 and data[i][j-1] == "#":
         change_direction("u")
@@ -201,7 +245,7 @@ for line in data:
             i = data.index(line)
             j = line.index(symbol)
             break
-print (right, left, down, up, i, j)
+
 
 
 while i != 0 and j != 0 and i != num_rows-1 and j != num_col-1:
@@ -217,7 +261,12 @@ while i != 0 and j != 0 and i != num_rows-1 and j != num_col-1:
 
 
 
-for string in data:
-    print (string)
 
+print(len(test))
+unique_arrays = []
+for array in test:
+    if array not in unique_arrays:
+        unique_arrays.append(array)
+
+print(len(unique_arrays))
 print (count)
